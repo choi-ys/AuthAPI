fde9d07b62435a98b5cab7c0d7a6a5b4022671b3
choi-ys <rcn115@naver.com>
2021-01-10 23:20:09 +0900

    [2021-01-10]용석: 회원 관련 파일 삭제
     - 회원 -> 제휴사 도메인 변경으로 인한 회원 관련 파일 삭

--- src/main/java/io/auth/api/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package io.auth.api.repository;
-
-import io.auth.api.domain.entity.MemberEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
-}
--- src/main/java/io/auth/api/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package io.auth.api.service;
-
-import io.auth.api.domain.common.ProcessingResult;
-import io.auth.api.domain.entity.MemberEntity;
-
-public interface MemberService {
-
-    ProcessingResult createMemberProcess(MemberEntity memberEntity);
-}
